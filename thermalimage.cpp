e95b4363d7d7305d4949f91a07943e31e6adf4f4
Niklas <krug@hm.edu>
2020-06-01 16:52:09 +0200

    created thermal Image class

--- thermalimage.cpp
+++ thermalimage.cpp
@@ -1,6 +1,90 @@
 #include "thermalimage.h"
 
-ThermalImage::ThermalImage(QWidget *parent) : QWidget(parent)
+
+float ThermalImage::getMin() const
+{
+    return minT;
+}
+
+float ThermalImage::getMax() const
+{
+    return maxT;
+}
+
+unsigned ThermalImage::getResX() const
+{
+    return resX;
+}
+
+unsigned ThermalImage::getResY() const
+{
+    return resY;
+}
+
+void ThermalImage::addPixel(int x, int y, float value)
+{
+    if(x >= 0 && x < static_cast<int>(resX))
+    {
+        if(y >= 0 && y < static_cast<int>(resY))
+        {
+            QVector<float>* vec = tempMap->data();
+            vec[x][y] = value;
+            if(value > maxT)
+            {
+                maxT = value;
+                emit newMinMax();
+            }
+            if(value < minT)
+            {
+                minT = value;
+                emit newMinMax();
+            }
+            int cindex = getPercent(value);
+            QColor c = colorGradient.at(cindex);
+            img->setPixelColor(x,y,c);
+        }
+    }
+}
+
+QImage *ThermalImage::getImg() const
+{
+    return img;
+}
+
+void ThermalImage::updateGradient()
 {
 
+}
+
+int ThermalImage::getPercent(const float &val)
+{
+    int perc = 0;
+    if(abs(maxT - minT) > 0.01)
+        perc = static_cast<int>(1/(maxT-minT)*(val-minT)*255);
+    return perc;
+}
+
+void ThermalImage::fillGradient()
+{
+    for(int i = 0; i <= 255; i++)
+    {
+        QColor c;
+        c.setRgb(i,0,255-i);
+        colorGradient.append(c);
+    }
+}
+
+ThermalImage::ThermalImage(unsigned resX_, unsigned resY_, QWidget *parent):resX(resX_), resY(resY_)
+{
+    img = new QImage(static_cast<int>(resX_),static_cast<int>(resY_),QImage::Format_RGB16);
+    img->fill(Qt::black);
+    tempMap = new QVector<QVector<float>> (static_cast<int>(resX_),QVector<float>(static_cast<int>(resY_)));
+    connect(this,SIGNAL(newMinMax()), this, SLOT(updateGradient()));
+    fillGradient();
+}
+
+ThermalImage::~ThermalImage()
+{
+    delete img;
+    delete [] tempMap;
 }
--- thermalimage.h
+++ thermalimage.h
@@ -6,12 +6,34 @@
 class ThermalImage : public QWidget
 {
     Q_OBJECT
+private:
+    QImage* img;
+    QVector<QVector<float>>* tempMap;
+    QList<QColor> colorGradient;
+    unsigned resX;
+    unsigned resY;
+    float minT = 20;
+    float maxT = 100;
+    int getPercent(const float& val); //0->255
+    void fillGradient();
 public:
-    explicit ThermalImage(QWidget *parent = nullptr);
+    explicit ThermalImage(unsigned resX_ = 1 , unsigned resY_ = 1,QWidget *parent = nullptr);
+    ~ThermalImage();
+    float getMin() const;
+    float getMax() const;
+    unsigned getResX() const;
+    unsigned getResY() const;
+    void addPixel(int x, int y, float value);
+    void addRow(int y, QVector<float>* row);
+    void addColumn(int x, QVector<float>* column);
+    QImage *getImg() const;
+    void getTemp(int x, int y);
 
 signals:
+    void newMinMax();
 
 public slots:
+    void updateGradient();
 };
 
 #endif // THERMALIMAGE_H
