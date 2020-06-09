c737c287373868d2e4ea86a85baaf848d1866ebb
Niklas <krug@hm.edu>
2020-06-09 17:27:45 +0200

    git added new command class
    Signed-off-by: Niklas <krug@hm.edu>

--- command.cpp
+++ command.cpp
@@ -1,6 +1,17 @@
 #include "command.h"
 
-command::command()
+unsigned command::getXPos() const
 {
+    return xPos;
+}
 
+unsigned command::getYPos() const
+{
+    return yPos;
+}
+
+command::command(int ctComm, unsigned deg_ ,unsigned xPos_,unsigned yPos_):xPos(xPos_), yPos(yPos_)
+{
+    deg = deg_;
+    comm = writeCalls[ctComm];
 }
--- command.h
+++ command.h
@@ -1,11 +1,28 @@
 #ifndef COMMAND_H
 #define COMMAND_H
+#include <QObject>
 
+const QStringList writeCalls = {"GR", "GT", "MR", "MT", "MM"};
 
 class command
 {
+    QString comm;
+    unsigned deg;
+    unsigned xPos;
+    unsigned yPos;
 public:
-    command();
+    explicit command(int ctComm, unsigned deg_ = 0,unsigned xPos_ = 0,unsigned yPos_ = 0);
+
+    const QString& getComm() const
+    {
+        return comm;
+    }
+    const unsigned& getDeg() const
+    {
+        return  deg;
+    }
+    unsigned getXPos() const;
+    unsigned getYPos() const;
 };
 
 #endif // COMMAND_H
