0897836ce76c317a81f1e1ac0e1bcc5ea2c38451
Niklas <krug@hm.edu>
2020-06-30 16:41:50 +0200

    added socket handling

---
+++ sockethandler.cpp
@@ -0,0 +1,6 @@
+#include "sockethandler.h"
+
+socketHandler::socketHandler(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ sockethandler.h
@@ -0,0 +1,16 @@
+#ifndef SOCKETHANDLER_H
+#define SOCKETHANDLER_H
+
+
+class socketHandler : public QObject
+{
+    Q_OBJECT
+public:
+    explicit socketHandler(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // SOCKETHANDLER_H
